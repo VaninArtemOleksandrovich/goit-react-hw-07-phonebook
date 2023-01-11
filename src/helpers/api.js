0c8656e3a3317a3bdca8964e5954a9f3f72e37a8
Ванин Артем <111682303+VaninArtemOleksandrovich@users.noreply.github.com>
2023-01-11 14:50:00 +0200

    /

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,19 +3,19 @@ import axios from 'axios';
 const BASE_URL = 'https://63beae78585bedcb36b3db15.mockapi.io';
 
 export const getContacts = async () => {
-  const { data } = await axios.get(`${BASE_URL}contacts`);
+  const { data } = await axios.get(`${BASE_URL}/contacts`);
 
   return data;
 };
 
 export const addContactsData = async contact => {
-  const { data } = await axios.post(`${BASE_URL}contacts`, contact);
+  const { data } = await axios.post(`${BASE_URL}/contacts`, contact);
 
   return data;
 };
 
 export const deleteContactsData = async contactId => {
-  const { data } = await axios.delete(`${BASE_URL}contacts/${contactId}`);
+  const { data } = await axios.delete(`${BASE_URL}/contacts/${contactId}`);
   console.log('data', data);
   return data;
 };
